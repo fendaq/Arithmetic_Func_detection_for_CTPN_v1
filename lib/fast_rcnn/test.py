a41fbfa33ae5d9956e00c37ba95b77c9e503822d
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-09-19 21:11:57 +0800

    加入归一化

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -48,7 +48,7 @@ def test_ctpn(sess, net, im, boxes=None):
     blobs, im_scales = _get_blobs(im, boxes)
 
     if cfg.TEST.HAS_RPN:
-        im_blob = blobs['data']
+        im_blob = blobs['data'] / 255 -1
         blobs['im_info'] = np.array(
             [[im_blob.shape[1], im_blob.shape[2], im_scales[0]]],
             dtype=np.float32)
